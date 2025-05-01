84dadb632e74f84a220e5c42aa68a1d77d392286
jepri simbolon <jepri.h.simbolon@gmail.com>
2025-05-02 03:03:43 +0700

    update tampilan login

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,11 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login - Kitakita Rent Car</title>
+    <!-- boostsrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <!-- css -->
+     <link rel="stylesheet" href="{{ asset('css/login.css') }}"
 </head>
+
 <body>
-    
+    <div class="container">
+        <div class="login">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="card">
+                        <form>
+                            <img src="{{ asset('asset/driver.jpeg') }}" alt="KitaKita Rent Car" class="d-block mx-auto">
+                            <div class="mb-3 email">
+                                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            </div>
+                            <div class="mb-3 password">
+                                <label for="exampleInputPassword1" class="form-label">Password</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1">
+                            </div>
+                            <div class="mb-3 text-end remember">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">Remember Me</label>
+                            </div>
+                            <button type="submit" class="btn d-block mx-auto">Log In</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
