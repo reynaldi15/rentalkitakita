e7de15eaa4689f8a51ec957b0eef3103dcd37f25
jepri simbolon <jepri.h.simbolon@gmail.com>
2025-05-01 11:15:03 +0700

    home 50%

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\ProductController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::middleware('guest')->group(function () {
